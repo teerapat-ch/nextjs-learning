fd7e5ed6a313134cb795bb2f5b4d7d6eff549529
teerapat-ch <coptee413@gmail.com>
2025-11-09 19:31:32 +0700

    check git username

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-primary text-5xl">Hello</h1>
+      <h1 className="text-primary text-5xl">Hello user!</h1>
     </>
   );
 }
